ef85b91a142dcc5d28a32af2d13bc53733972a0b
lvnini <lv@lvnini.cn>
2018-07-27 09:45:53 +0800

    完成主题页与数据渲染

---
+++ Protoss/pages/theme/theme-model.js
@@ -0,0 +1,25 @@
+
+import { Base } from '../../utils/base.js';
+
+class Theme extends Base {
+
+  constructor() {
+    super();
+  }
+
+  // 获取主题下的数据
+  getProductsData(id, callback){
+    var param = {
+      url: 'theme/' + id,
+      sCallback:function(data){
+        callback && callback(data);
+      }
+    };
+    this.request(param);
+  }
+
+
+
+}
+
+export { Theme };
